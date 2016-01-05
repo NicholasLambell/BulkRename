0a611812390264accd89eb279a02b1e1efafb033
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-05 16:09:16 +1000

    Added Util class with some file path utility methods

---
+++ BulkRename/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkRename {
+    class Util {
+        public static string GetExe() {
+            return System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+
+        public static string FileName(string input) {
+            if (File.Exists(input)) {
+                return Path.GetFileNameWithoutExtension(new FileInfo(input).Name);
+            }
+            return null;
+        }
+    }
+}
