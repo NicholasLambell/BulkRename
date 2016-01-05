48844e6a54c54dbb6836420398b898a4d9bafcc7
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-05 14:07:46 +1000

    Initialize repository

---
+++ BulkRename/FormMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BulkRename {
+    public partial class FormMain : Form {
+        string[] _args;
+        List<FileInfo> _fileDirs;
+
+        public FormMain() {
+            InitializeComponent();
+
+            //Initialize files list
+            _fileDirs = new List<FileInfo>();
+
+            //Get command line arguments and process them
+            _args = Environment.GetCommandLineArgs();
+            ProcessArgs();
+        }
+
+        private void ProcessArgs() {
+            if (_args.Length > 0) {
+                foreach (string arg in _args) {
+                    if (File.Exists(arg)) {
+
+                    }
+                }
+            }
+        }
+    }
+}
