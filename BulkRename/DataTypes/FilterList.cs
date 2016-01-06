dbd28b105d159cf04d6f889f336781c131d15175
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-07 02:01:30 +1000

    Changed public field names

---
+++ BulkRename/DataTypes/FilterList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkRename {
+    public class FilterList {
+        public List<DefaultFilters> defaultFilters;
+        public List<string[]> customFilters;
+
+        public FilterList(List<DefaultFilters> defaultFilters, List<string[]> customFilters) {
+            this.defaultFilters = defaultFilters;
+            this.customFilters = customFilters;
+        }
+    }
+}
