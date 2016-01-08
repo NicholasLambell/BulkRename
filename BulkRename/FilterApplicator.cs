2ac44a80976eef55da97bcd734a9a472fe3b93cc
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-09 01:49:15 +1000

    Began writing methods

--- BulkRename/FilterApplicator.cs
+++ BulkRename/FilterApplicator.cs
@@ -8,20 +8,38 @@ namespace BulkRename {
     class FilterApplicator {
         #region Default Filters
         public static string ReplacePeriods(string fileName) {
-            return null;
+            return fileName.Replace(".", " ");
         }
 
         public static string CapitalizeAll(string fileName) {
+            //Split name into individual words
+            string[] words = Util.SplitWords(fileName);
+
+            //Cycle through words and capitalize all words
+            for (int i = 0; i < words.Length; i++) {
+
+            }
+
             return null;
         }
 
         public static string CapitalizeKey(string fileName) {
+            string[] noCap = { "a", "an", "the", "and", "but", "or", "nor", "at", "by", "for", "from", "in", "into", "of", "off", "on", "up", "with", "to", "as", "o,Clock" };
+
+            //Split name into individual words
+            string[] words = Util.SplitWords(fileName);
+
+            //Cycle through words and capitalize all key words
+            for (int i = 0; i < words.Length; i++) {
+
+            }
+
             return null;
         }
         #endregion
 
         public static string CustomFilter(string fileName, string filterIn, string filterOut) {
-            return null;
+            return fileName.Replace(filterIn, filterOut);
         }
     }
 }
