7dae7557114b3fe740a327a7b9f3ccaf46756cc1
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-09 00:58:33 +1000

    Created empty file handler class to log errors to file

--- BulkRename/FilterHandler.cs
+++ BulkRename/FilterHandler.cs
@@ -7,18 +7,57 @@ using System.Threading.Tasks;
 
 namespace BulkRename {
     class FilterHandler {
-        private List<FileInfo> _files;
+        private static List<string> _renameFiles;
 
-        public void ProcessFilters(FilterList filterList, List<FileInfo> files) {
-            _files = files;
+        public static void ProcessFilters(FilterList filterList, List<FileInfo> files) {
+            //Initialize list of file names
+            _renameFiles = new List<string>();
+            foreach (FileInfo info in files) {
+                _renameFiles.Add(Util.FileName(info.Name));
+            }
+
+            //Run filter processors
+            ProcessDefaultFilters(filterList.defaultFilters);
+            ProcessCustomFilters(filterList.customFilters);
+
+            //Rename files
+            if (files.Count == _renameFiles.Count) {
+                for (int i = 0; i < files.Count; i++) {
+                    if (Util.FileName(files[i].Name) != _renameFiles[i]) {
+                        FileHandler.Rename(files[i].FullName, _renameFiles[i]);
+                    }
+                }
+            } else {
+                ErrorHandler.LogError("File and Rename count missmatch, please try again.", true);
+            }
         }
 
-        private void ProcessDefaultFilters(List<DefaultFilters> filters) {
+        private static void ProcessDefaultFilters(List<DefaultFilters> filters) {
+            for (int i = 0; i < _renameFiles.Count; i++) {
+                foreach (DefaultFilters filter in filters) {
+                    switch (filter) {
+                        case DefaultFilters.ReplacePeriods:
+                            _renameFiles[i] = FilterApplicator.ReplacePeriods(_renameFiles[i]);
+                            break;
 
+                        case DefaultFilters.CapitalizeAll:
+                            _renameFiles[i] = FilterApplicator.CapitalizeAll(_renameFiles[i]);
+                            break;
+
+                        case DefaultFilters.CapitalizeKey:
+                            _renameFiles[i] = FilterApplicator.CapitalizeKey(_renameFiles[i]);
+                            break;
+                    }
+                }
+            }
         }
 
-        private void ProcessCustomFilters(List<string[]> filters) {
-
+        private static void ProcessCustomFilters(List<string[]> filters) {
+            for (int i = 0; i < _renameFiles.Count; i++) {
+                foreach (string[] filter in filters) {
+                    _renameFiles[i] = FilterApplicator.CustomFilter(_renameFiles[i], filter[0], filter[1]);
+                }
+            }
         }
     }
 }
