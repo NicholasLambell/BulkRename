b968da4b93855b6540dde4d13bf4c9543c618823
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-09 01:48:55 +1000

    Added methods and null checks

--- BulkRename/Util.cs
+++ BulkRename/Util.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BulkRename {
     class Util {
+        #region File Related Methods
         public static string GetExe() {
             return System.Reflection.Assembly.GetExecutingAssembly().Location;
         }
 
         public static string FileName(string input) {
-            if (File.Exists(input)) {
+            if (!String.IsNullOrEmpty(input) && File.Exists(input)) {
                 string output = Path.GetFileNameWithoutExtension(new FileInfo(input).Name);
                 return System.Diagnostics.Debugger.IsAttached ? output.Replace(".vshost", "") : output;
             }
+            return input;
+        }
+        #endregion
+
+        public static string[] SplitWords(string input) {
+            if (!String.IsNullOrEmpty(input)) {
+                return Regex.Split(input, " ");
+            }
             return null;
+        }
+
+        public static string Capitalize(string input) {
+            if (!String.IsNullOrEmpty(input)) {
+                return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input);
+            }
+            return input;
         }
     }
 }
