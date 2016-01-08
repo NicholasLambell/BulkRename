36e9aeab02e4ff3c93f50113716d0d6cfa3a451f
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-09 00:59:12 +1000

    Added class with stub methods for applying filters to strings

---
+++ BulkRename/FilterApplicator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkRename {
+    class FilterApplicator {
+        #region Default Filters
+        public static string ReplacePeriods(string fileName) {
+            return null;
+        }
+
+        public static string CapitalizeAll(string fileName) {
+            return null;
+        }
+
+        public static string CapitalizeKey(string fileName) {
+            return null;
+        }
+        #endregion
+
+        public static string CustomFilter(string fileName, string filterIn, string filterOut) {
+            return null;
+        }
+    }
+}
