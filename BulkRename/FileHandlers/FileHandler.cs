84fbb63a76adfd35ecd100bea4de96de2b64d635
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-14 20:31:50 +1000

    Improved code for handling file name iteration

--- BulkRename/FileHandlers/FileHandler.cs
+++ BulkRename/FileHandlers/FileHandler.cs
@@ -9,24 +9,28 @@ namespace BulkRename {
     class FileHandler {
         public static bool Rename(string filePath, string rename) {
             if (!String.IsNullOrEmpty(filePath) && !String.IsNullOrEmpty(rename) && File.Exists(filePath)) {
+                FileInfo file = null;
+
                 try {
-                    FileInfo file = new FileInfo(filePath);
-                    int iteration = 2;
-                    string sIteration = "";
-
-                    //Check to make sure file doesn't already exist with that name and auto incriment number if it does
-                    //while (File.Exists(file.Directory.FullName + @"\" + rename + sIteration + file.Extension)) {
-                    //    sIteration = String.Format(" ({0})", iteration);
-                    //    iteration++;
-                    //}
-                    rename += sIteration;
-
+                    file = new FileInfo(filePath);
                     file.MoveTo(file.Directory.FullName + @"\" + rename + file.Extension);
                     return true;
 
                 } catch (IOException ex) {
                     ErrorHandler.LogError(ex.GetType().ToString(), true);
 
+                    if (file != null && File.Exists(file.Directory.FullName + @"\" + rename + file.Extension)) {
+                        int iteration = 2;
+                        string sIteration = "";
+
+                        //Continue incrimenting the iteration until no file with name exists
+                        while (File.Exists(file.Directory.FullName + @"\" + rename + sIteration + file.Extension)) {
+                            sIteration = String.Format(" ({0})", iteration);
+                            iteration++;
+                        }
+
+                        return Rename(filePath, rename + sIteration);
+                    }
                 } catch (Exception ex) {
                     ErrorHandler.LogError(ex.GetType() + ": " + ex.Message, true);
                 }
