06732aef9d2271afd50bd8d708badce77dc758df
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-22 02:13:02 +1000

    Added method to merge FilterList objects and ignore duplicate filters

--- BulkRename/DataTypes/FilterList.cs
+++ BulkRename/DataTypes/FilterList.cs
@@ -13,5 +13,22 @@ namespace BulkRename {
             this.defaultFilters = defaultFilters;
             this.customFilters = customFilters;
         }
+
+        public FilterList Merge(FilterList filterList) {
+            if (filterList != null) {
+                foreach (DefaultFilters filter in filterList.defaultFilters) {
+                    if (!this.defaultFilters.Contains(filter)) {
+                        this.defaultFilters.Add(filter);
+                    }
+                }
+
+                foreach (string[] filter in filterList.customFilters) {
+                    if (this.customFilters.FindIndex(item => item[0] == filter[0] && item[1] == filter[1]) < 0) {
+                        this.customFilters.Add(filter);
+                    }
+                }
+            }
+            return this;
+        }
     }
 }
