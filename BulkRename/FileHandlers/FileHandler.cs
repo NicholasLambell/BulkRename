67a58081199bba4576c113c13812d4e5e40d6c31
Nicholas Lambell <lambell.nicholas95@gmail.com>
2016-01-21 16:08:27 +1000

    Removed old code, finalized iteration handling and commented

--- BulkRename/FileHandlers/FileHandler.cs
+++ BulkRename/FileHandlers/FileHandler.cs
@@ -11,24 +11,23 @@ namespace BulkRename {
         public static bool Rename(FileInfo file, string rename) {
             if (file != null && file.Exists && !String.IsNullOrEmpty(rename)) {
                 int iteration = 1;
-
-                //Regex regex = new Regex(@" (\d{})");
                 string fileName = Util.FileName(file.Name);
 
-                //if (regex.IsMatch(fileName)) {
-                //    Match match = regex.Match(fileName);
-                //    string sMatch = match.Value;
-
-                //    string sIteration = (fileName.Length >= 4) ? fileName.Substring(fileName.Length - 4) : "";
-
-                //    iteration = !String.IsNullOrEmpty(sIteration) ? Convert.ToInt32(sIteration.Substring(2, 1)) : iteration;
-                //}
-
+                //Try to get iteration from file name if it's at least 4 chars long
                 if (fileName != null && fileName.Length >= 4) {
                     int iterationGet = GetIteration(fileName);
                     iteration = (iterationGet > iteration) ? iterationGet : iteration;
                 }
 
+                //If the rename string contains the iteration patern eg " (2)" remove it.
+                if (iteration >= 2) {
+                    Regex regex = new Regex(@" \(\d+\)$");
+                    if (regex.IsMatch(rename)) {
+                        rename = regex.Replace(rename, "");
+                    }
+                }
+
+                //Loop through iterations trying to rename until a value one is found
                 while (!ApplyRename(file, rename, iteration)) {
                     iteration++;
                 }
@@ -36,47 +35,18 @@ namespace BulkRename {
             }
             return false;
         }
-
-        //public static bool Rename(string filePath, string rename) {
+        
         private static bool ApplyRename(FileInfo file, string rename, int iteration) {
-            //if (!String.IsNullOrEmpty(filePath) && !String.IsNullOrEmpty(rename) && File.Exists(filePath)) {
             if (file != null && file.Exists && !String.IsNullOrEmpty(rename)) {
-                //FileInfo file = null;
-
                 try {
-                    //string sIteration = (iteration >= 2) ? String.Format(" ({0})", iteration) : "";
-                    string sIteration = "";
-
-                    if (iteration >= 2) {
-                        sIteration = String.Format(" ({0})", iteration);
-
-                        Regex regex = new Regex(@" \(\d+\)$");
-                        if (regex.IsMatch(rename)) {
-                            rename = regex.Replace(rename, "");
-                        }
-                    }
-
-
-                    //file = new FileInfo(filePath);
+                    //Check iteration and form iteration string
+                    string sIteration = (iteration >= 2) ? String.Format(" ({0})", iteration) : "";
+                    
+                    //Try to rename the file
                     file.MoveTo(file.Directory.FullName + @"\" + rename + sIteration + file.Extension);
                     return true;
 
-                } catch (IOException ex) {
-                    //ErrorHandler.LogError(ex.GetType().ToString(), true);
-
-                    //if (file != null && File.Exists(file.Directory.FullName + @"\" + rename + file.Extension)) {
-                    //    int iteration = 2;
-                    //    string sIteration = "";
-
-                    //    //Continue incrimenting the iteration until no file with name exists
-                    //    while (File.Exists(file.Directory.FullName + @"\" + rename + sIteration + file.Extension)) {
-                    //        sIteration = String.Format(" ({0})", iteration);
-                    //        iteration++;
-                    //    }
-
-                    //    //return Rename(filePath, rename + sIteration);
-                    //    return Rename(file, rename + sIteration);
-                    //}
+                } catch (IOException) { //Triggered if file exists, goes back to while loop above
                 } catch (Exception ex) {
                     ErrorHandler.LogError(ex.GetType() + ": " + ex.Message, true);
                 }
@@ -85,8 +55,11 @@ namespace BulkRename {
         }
 
         private static int GetIteration(string fileName) {
+            //If the last characters of the string are "(number)" extract the number
             Regex regex = new Regex(@"(?<=\()\d+(?=\)$)");
             Match match = regex.Match(fileName);
+
+            //Return the extracted number or if empty or not a number return 0
             int output;
             return (match.Groups[0].Value != String.Empty && Int32.TryParse(match.Groups[0].Value, out output)) ? output : 0;
         }
